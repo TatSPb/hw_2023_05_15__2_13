91e340652723799d5b0c9bf97eec87bcfbcdfade
TatSPb <t-sverchkova@yandex.ru>
2023-06-23 14:03:21 +0300

    Commit-1

---
+++ src/main/java/hw_2023_05_15__2_13/Hw20230515213Application.java
@@ -0,0 +1,13 @@
+package hw_2023_05_15__2_13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw20230515213Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw20230515213Application.class, args);
+    }
+
+}
---
+++ src/test/java/hw_2023_05_15__2_13/Hw20230515213ApplicationTests.java
@@ -0,0 +1,13 @@
+package hw_2023_05_15__2_13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw20230515213ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
